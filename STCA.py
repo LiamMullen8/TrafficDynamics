84cb1e4b04f97f40138fb48ec894b0592b94c65d
Liam Mullen <liammullen0@gmail.com>
2022-01-01 14:39:53 -0800

    need to fix flux and density

--- STCA.py
+++ STCA.py
@@ -27,6 +27,7 @@ def initialize_cars(car_length, num_cars):
     for i in range(0, num_cars):
         print(f"Initial Car {i} Stats: \n Length: {Cars[i].length} \n Position: {Cars[i].position} \n Velocity: {Cars[i].velocity} \n Gap: {Cars[i].gap}")
     print(f"END WALL: Position: {Cars[num_cars].position}")
+    print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 
     return
 
@@ -36,27 +37,33 @@ def update_cars(rand_prob, dt):
     for car in (Cars[::-1])[1:]:
 
         ##############################################
-        # 4 RULES OF MOTION
+        # RULES OF MOTION
         
         # speedup
         if car.velocity != max_velocity:
             car.velocity += dt
 
+        # randomize changing speed of cars bc of environ and such
+        if car.velocity != 0:
+           if random.random() < rand_prob:
+               car.velocity -= dt
+           else:
+               car.velocity += dt
+
+        # follow speed limit
+        if car.velocity > max_velocity:
+            car.velocity = max_velocity
+
         # dont hit each other
         if car.velocity > car.gap:
             car.velocity = car.gap
 
-        # randomize changing speed of cars bc of environ and such
-        if car.velocity != 0:
-           if random.random() < rand_prob:
-               car.velocity -= random.random()
-    
         # move the car to new position
         car.position += car.velocity
         ##############################################
-        
+
         # update gap between cars
-        car.gap = Cars[Cars.index(car)+1].position - (car.position+1)
+        car.gap = Cars[Cars.index(car)+1].position - (car.position + L)
         
     return
 
@@ -70,14 +77,16 @@ def display_stats():
 def measure_density(x0, dx, t0):
 
     cars_in_interval = list(filter(lambda car: (x0-dx <= car.position <= x0+dx), Cars[:-1]))
-    density = len(cars_in_interval) / (2 * dx)
+    density = float(len(cars_in_interval)) / (2.0 * dx)
     print(f"density @ x0, t0: {(x0,t0)}: {density}")
+    return density
 
 def measure_flux(x0, t0, dt):
 
     cars_in_interval = list(filter(lambda car: ((car.position - car.velocity*dt) < x0 < (car.position + car.velocity*dt)), Cars[:-1]))
-    flux = len(cars_in_interval) / 2*dt
+    flux = float(len(cars_in_interval)) / (2.0 * dt)
     print(f"flux @ x0,t0: {(x0,t0)}: {flux}")
+    return flux
 
 
     
@@ -88,33 +97,71 @@ if __name__ == "__main__":
     dt = float(input("Enter timestep: "))
     M = float(input("Enter Max Velocity: "))
     EW = float(input("Enter End Wall Distance: "))
-    max_velocity = float(M * L) / dt
+    D_range = (int(input("Enter x0 and dx for Density: ")), float(input("")))
+    F_range = (int(input("Enter x0 and dt for Flux: ")), float(input("")))
+    f = int(input("#frames:"))
+    
+    max_velocity = M #float(M * L) / dt
 
     Cars = []
 
 
-
     initialize_cars(L, N)
 
-    # setup graph
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(120, 10))
 
-    axes.set_xlabel("Time (t)")
-    axes.set_ylabel("Position (x)")
+    ###################################################################################
+    # # setup graph # #
+    
+    fig, (flux_ax, dens_ax, car_ax) = plt.subplots(nrows=3, ncols=1, figsize=(120, 10))
+
+    car_ax.set_title("Car Positions")
+    car_ax.set_xlabel("Time (t)")
+    car_ax.set_ylabel("Position (x)")
 
     # set limit for x and y axis
-    axes.set_ylim(-100, EW+10)
-    axes.set_xlim(0, 500)
-
+    #car_ax.set_ylim(-EW / 2, EW+10)
+    car_ax.set_xlim(0, f)
 
     # store car position after every time step
     x1, y1 = [0], [[car.position] for car in Cars[:-1]]
 
     # graph line for each car
-    P = [axes.plot(x1, car) for car in y1]
+    car_lines = [car_ax.plot(x1, car) for car in y1]
 
+
+    # Working range for Density 
+    #car_ax.axhline(D_range[0]-D_range[1], color="r")
+    #car_ax.axhline(D_range[0]+D_range[1],color="r")
+    car_ax.fill_between(np.arange(0,f+1), D_range[0]-D_range[1], D_range[0]+D_range[1], alpha=0.2, color='b')
+
+    # Working range for flux
+    #car_ax.axvline(F_range[0]-F_range[1])
+    #car_ax.axvline(F_range[0]+F_range[1])
+    #car_ax.axhline(F_range[0], F_range[0]-F_range[1], F_range[0]+F_range[1])
+    # car_ax.fill_between(np.arange(0,f+1), where=D_range[0]-D_range[1], D_range[0]+D_range[1], alpha=0.2, color='b')
+    
+    Dens = dens_ax.plot([],[], lw=2, color='b')
+    Dens_x,Dens_y = [],[]
+    
+    #dens_ax.set_ylim(0, .25)
+    dens_ax.set_xlim(0, f)
+    dens_ax.set_xlabel("Time (t)")
+    dens_ax.set_ylabel("Density (p rho)")
+    dens_ax.set_title(f"Density at position x0={D_range[0]}, dx={D_range[1]}")
+
+    Flux = flux_ax.plot([],[], lw=2, color='g')
+    Flux_x, Flux_y = [],[]
+
+    #flux_ax.set_ylim(0, 5)
+    flux_ax.set_xlim(0, f)
+    flux_ax.set_xlabel("Time (t)")
+    flux_ax.set_ylabel("Flux (J)")
+    flux_ax.set_title(f"Flux at position x0={F_range[0]}, dt={F_range[1]}")
+    ####################################################################################
     
     def animate(i):
+
+        update_cars(.1, dt)
 
         ##########
         # add coords of each car to graph respective line
@@ -124,26 +171,37 @@ if __name__ == "__main__":
             y1[o].append(Cars[o].position)
         ##########
 
-        display_stats()
-        update_cars(.3, dt)
 
         ##########
         # update each line of graph
-        for L in range(len(P)):
-            P[L][0].set_data(x1[:i],y1[L][:i])
+        for L in range(len(car_lines)):
+            car_lines[L][0].set_data(x1[:i],y1[L][:i])
         ##########
-        
+
         # sort cars by pos so each can react to the car immediately in front
-        Cars.sort(key=lambda x: x.position)
+        #Cars[:-1] = sorted(Cars[:-1], key=lambda x: x.position)
+
+        #display_stats()
 
         # math
-        measure_density(x0=25, dx=10, t0=i)
-        measure_flux(x0=50, t0=i, dt=2.5)
+        density = measure_density(x0=25, dx=10, t0=i)
+        Dens_x.append(i)
+        Dens_y.append(density)
+        
+        flux = measure_flux(x0=50, t0=i, dt=2.5)
+        Flux_x.append(i)
+        Flux_y.append(flux)
 
+        Dens[0].set_data(Dens_x[:i], Dens_y[:i])
+        Flux[0].set_data(Flux_x[:i], Flux_y[:i])
+        
+        car_ax.set_ylim(min(y1[0])-10, EW+10)     
+        dens_ax.set_ylim(0, max(Dens_y)+.1)     
+        flux_ax.set_ylim(0, max(Flux_y)+.1)     
+        
     # animation driver    
-    anim = FuncAnimation(fig, animate, frames=int(input("#frames:")), interval=1, repeat=False)
+    anim = FuncAnimation(fig, animate, frames=f, interval=1, repeat=True)
     
     # save animation
-    writervideo = animation.FFMpegWriter(fps=60)
-    anim.save('Noncolliding.mp4', writer=writervideo)
+    anim.save('Noncolliding.mp4', fps=60)
     plt.show()
